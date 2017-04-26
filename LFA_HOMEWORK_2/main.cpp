5052165d10d140dcaf7f7dea4d86dfa991c2a933
Marius Ilie <mariusjcb@Mac-Pro.local>
2017-04-26 12:52:53 -0700

    read final states

--- LFA_HOMEWORK_2/main.cpp
+++ LFA_HOMEWORK_2/main.cpp
@@ -23,28 +23,62 @@ struct Edge {
     int end;
 };
 
+char* readBufferFromConsoleInput() {
+    static char buff[125];
+    
+    fgets(buff, sizeof(buff), stdin);
+    return strtok(buff, " ");
+}
+
 int readStartState(ifstream *file = NULL) {
     if(file == NULL)
     {
-        char buff[125];
-    
-        fgets(buff, sizeof(buff),stdin);
-        char *p = strtok(buff, " ");
-    
-        return atoi(p);
+        return atoi(readBufferFromConsoleInput());
     } else {
         int x = -1;
         *file >> x;
+        
         return x;
     }
 }
 
-int main(int argc, const char * argv[])
-{
+vector<int> readFinalStates(ifstream *file = NULL) {
+    vector<int> finalStates;
+    char *p = NULL;
+    
+    if(file == NULL)
+    {
+        p = readBufferFromConsoleInput();
+    } else {
+        string line;
+        getline(*file, line);
+        
+        p = strtok(strdup(line.c_str()), " ");
+    }
+    
+    while (p != NULL)
+    {
+        int state = atoi(p);
+        finalStates.push_back(state);
+        
+        p = strtok(NULL, " ");
+    }
+    
+    return finalStates;
+}
+
+int main(int argc, const char * argv[]) {
     vector<Edge> edges;
     
     ifstream f("inputs.txt");
-    int start_state = readStartState(&f);
+    
+    int start_state = readStartState(stdin == NULL ? &f : NULL);
+    vector<int> final_states = readFinalStates(stdin == NULL ? &f : NULL);
+    
+    cout << start_state << "\n";
+    for(int i = 0; i < final_states.size(); i++) {
+        cout << final_states[i] << " ";
+    }
     
     return 0;
 }
