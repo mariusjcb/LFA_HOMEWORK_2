d39dd15e8369f072b1c80a4bfe808e32378d147c
Marius Ilie <mariusjcb@Mac-Pro.local>
2017-04-26 12:08:16 -0700

    Initial code

--- LFA_HOMEWORK_2/main.cpp
+++ LFA_HOMEWORK_2/main.cpp
@@ -6,10 +6,45 @@
 //  Copyright © 2017 Marius Ilie. All rights reserved.
 //
 
-#include <iostream>
+//MARK: C libraries
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//MARK: C++ libraries
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+struct Edge {
+    int start;
+    char key;
+    int end;
+};
+
+int readStartState(ifstream *file = NULL) {
+    if(file == NULL)
+    {
+        char buff[125];
+    
+        fgets(buff, sizeof(buff),stdin);
+        char *p = strtok(buff, " ");
+    
+        return atoi(p);
+    } else {
+        int x = -1;
+        *file >> x;
+        return x;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    vector<Edge> edges;
+    
+    ifstream f("inputs.txt");
+    int start_state = readStartState(&f);
+    
     return 0;
 }
