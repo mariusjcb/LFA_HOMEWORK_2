8c2706406ede80f442ce89d9eb1f345595935559
Marius Ilie <mariusjcb@Mac-Pro.local>
2017-04-26 11:35:31 -0700

    Initial Commit

---
+++ LFA_HOMEWORK_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LFA_HOMEWORK_2
+//
+//  Created by Marius Ilie on 26/04/2017.
+//  Copyright © 2017 Marius Ilie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
